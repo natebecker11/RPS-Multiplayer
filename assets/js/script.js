32e4a46da829091194b2161cedd209b7e0280d23
natebecker11 <natebecker11@gmail.com>
2018-09-10 22:04:33 -0400

    firebase snippets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 var database = firebase.database();
+
+
+// // auth code snippets
+// function signIn() {
+//   var provider = new firebase.auth.GoogleAuthProvider();
+//   firebase.auth().signInWithPopup(provider);
+// }
+
+// // Signs-out of Friendly Chat.
+// function signOut() {
+//   firebase.auth().signOut();
+// }
+
+// // Initiate firebase auth.
+// function initFirebaseAuth() {
+//   // Listen to auth state changes
+//   firebase.auth().onAuthStateChanged(cb);
+// }
+
+
+// // User data location
+// var currentUser = firebase.auth().currentUser
