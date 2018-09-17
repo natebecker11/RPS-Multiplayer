526c03f3980a2300df76ba58a8a591c4e87e70f1
natebecker11 <natebecker11@gmail.com>
2018-09-16 22:45:49 -0400

    ingame auth for user1/user2

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,70 +52,18 @@ connectedRef.on('value', function(snap) {
 
 
 
-// when the login button is clicked
-document.querySelector('#loginButton').onclick = function(event) {
-  // prevent page reload on click
-  event.preventDefault();
-  // var for entered username
-  var userDisplayName = document.querySelector('#loginText').value;
-  
-  // if a value is entered
-  if (userDisplayName) {
-    // display the user's name 
-    document.querySelector('#userName').textContent = userDisplayName;
-    // flag that the name is displayed
-    nameDisplayed = true;
-    // run the firebase anon auth method
-    fAuth.signInAnonymously().then(function(snap) {
-        var uid = snap.user.uid;        
-        // on the database
-        database.ref('/' + uid + '/info/').set({
-            displayName: userDisplayName,
-            userID: uid
-          })
-        fAuth.currentUser.updateProfile({
-          displayName: userDisplayName
-        })
-      })
-      // catch errors
-      .catch(function(error) {      
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode + errorMessage);
-    })
-  } else {
-    alert('Please enter a name!')
-  }
-}
+
 
 // fAuth listener for login status change
 fAuth.onAuthStateChanged(function(user) {  
-  if (user) {
-    // user is signed in
-    // var userDisplayName = document.querySelector('#loginText').value;
-    // if (userDisplayName) {
-    //   user.updateProfile({
-    //       displayName: userDisplayName
-    //   })
-    // }
-    // console.log('')
+  if (user) {    
     if (!nameDisplayed) {
       console.log('displaying user name')
       document.querySelector('#userName').textContent = fAuth.currentUser.displayName;
-    }
-    
-    console.log('dingo')
-    
+    }    
     // document.querySelector('#userName').textContent = database.ref('/' + user.uid + '/').displayName;
     document.querySelector('#loginScreen').classList.add('hidden');
     document.querySelector('#waitScreen').classList.remove('hidden');
-
-    // check to see if there is room for the user
-      // send to game if yes
-    // else
-      // try again button
-    
-    
   } else {
     console.log('signed out')
   }
@@ -174,7 +122,26 @@ var gameReadyCheck = function() {
 
 
 // function to send the user's choice to the server
+var chooseRPS = function(choice) {
+  console.log(choice)
+  database.ref('/currentGame/')
+}
 
+// function to authenticate which user a user is
+var authUserNum = function() {
+  var currentID = fAuth.currentUser.uid;
+  var userNumber;
+  var currentGame = database.ref('/currentGame/');
+  currentGame.once('value', function(snap) {
+    for (var user in snap.val()) {
+      // console.log(snap.val()[user]['userID']);
+      if (snap.val()[user]['userID'] === currentID) {
+        return userNumber = user;
+      }
+    } 
+  })
+  return userNumber;
+}
 
 // function to check whether both choices have been entered
 
@@ -201,25 +168,51 @@ database.ref('/currentGame/').on('value', function(snap) {
 // listener for Start Game button
 document.querySelector('#startBtn').onclick = function() {gameReadyCheck()}
 
+// listener for the Login button
+document.querySelector('#loginButton').onclick = function(event) {
+  // prevent page reload on click
+  event.preventDefault();
+  // var for entered username
+  var userDisplayName = document.querySelector('#loginText').value;
+  
+  // if a value is entered
+  if (userDisplayName) {
+    // display the user's name 
+    document.querySelector('#userName').textContent = userDisplayName;
+    // flag that the name is displayed
+    nameDisplayed = true;
+    // run the firebase anon auth method
+    fAuth.signInAnonymously().then(function(snap) {
+        var uid = snap.user.uid;        
+        // on the database
+        database.ref('/' + uid + '/info/').set({
+            displayName: userDisplayName,
+            userID: uid
+          })
+        fAuth.currentUser.updateProfile({
+          displayName: userDisplayName
+        })
+      })
+      // catch errors
+      .catch(function(error) {      
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + errorMessage);
+    })
+  } else {
+    alert('Please enter a name!')
+  }
+}
 
 
+// listener for the gameplay buttons
+var gameplayBtns = document.querySelectorAll('.gameplayBtn')
+gameplayBtns.forEach(function(element) {
+  element.addEventListener('click', function(event) {
+    chooseRPS(this.textContent)
+  })
+})
 
-// // auth code snippets
-// function signIn() {
-//   var provider = new firebase.auth.GoogleAuthProvider();
-//   firebase.auth().signInWithPopup(provider);
-// }
-
-// // Signs-out of Friendly Chat.
-// function signOut() {
-//   firebase.auth().signOut();
-// }
-
-// // Initiate firebase auth.
-// function initFirebaseAuth() {
-//   // Listen to auth state changes
-//   firebase.auth().onAuthStateChanged(cb);
-// }
 
 
 // // User data location
